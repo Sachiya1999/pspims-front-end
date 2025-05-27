9b037a74718c37250dbe0773f6246178054b3f72
Sachiya1999 <sachintha.damian@gmail.com>
2025-05-27 20:26:14 +0530

    update app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+      { path: 'login', loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule) }
+    ]
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+     ]
+  },
+  { path: '**', component: NotFoundComponent }
+];
